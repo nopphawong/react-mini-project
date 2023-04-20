1206b1fa3f11dc5520c4fbffa343c916e83302ed
nopphawong.n <nopphawong.n@gmail.com>
2023-04-20 20:18:49 +0700

    mini project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Provider } from 'react-redux'
@@ -8,18 +7,27 @@ import store from './store/store'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import MovieDetail from './components/MovieDetail/MovieDetail'
+import NoteFound from './components/NotFound/NoteFound'
+import Footer from './components/Footer/Footer'
 
 function App() {
+  document.title = 'OMDb API'
   return (
     <div>
       <Provider store={store}>
         <BrowserRouter>
-          <Header />
+          <div className='header'>
+            <Header />
+          </div>
           <div className='container'>
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/movie/:id' element={<MovieDetail />} />
+              <Route path='*' element={<NoteFound />} />
             </Routes>
+          </div>
+          <div className='footer'>
+            <Footer />
           </div>
         </BrowserRouter>
       </Provider>
