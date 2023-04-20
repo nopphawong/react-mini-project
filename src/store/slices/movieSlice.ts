14fe538be819dd712c762a05aaed2a37a6f03238
nopphawong.n <nopphawong.n@gmail.com>
2023-04-20 22:22:03 +0700

    mini project

--- src/store/slices/movieSlice.ts
+++ src/store/slices/movieSlice.ts
@@ -1,19 +1,19 @@
 import { createSlice, current } from '@reduxjs/toolkit'
 import { SearchMovieType } from '../../models/movie.model'
 
-const initialState = {
-    movies: []
+const initialState: SearchMovieType = {
+  movies: [],
 }
 
 const movieSlice = createSlice({
-    name: "movieListing",
-    initialState,
-    reducers: {
-        addMovie: (state, action) => {
-            state.movies = action.payload
-            console.log(current(state))
-        }
-    }
+  name: 'movieListing',
+  initialState,
+  reducers: {
+    addMovie: (state, action) => {
+      state.movies = action.payload
+      console.log(current(state))
+    },
+  },
 })
 
 export const { addMovie } = movieSlice.actions
